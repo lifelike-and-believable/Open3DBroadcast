382deef9ad48bcc45fc142ea53aafaeba147173c
Athomas Goldberg <athomas.goldberg@shocap.com>
2025-10-29 16:26:59 -0400

    Add ProjectSandbox Unreal project with starter module, assets, maps and config
    - Add ProjectSandbox.uproject and module boilerplate (Build.cs, module .cpp/.h, Target files)
    - Add simple DoNothing class and primary game module implementation
    - Add .ignore to exclude editor/derived and plugin folders
    - Set EditorStartupMap to /Game/TestMap.TestMap in DefaultEngine.ini
    - Add large collection of Content assets (maps, blueprints, animations, meshes, materials, inputs, tests) for demo/prototyping

---
+++ ProjectSandbox/Source/ProjectSandbox/DoNothing.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class PROJECTSANDBOX_API DoNothing
+{
+public:
+	DoNothing();
+	~DoNothing();
+};
