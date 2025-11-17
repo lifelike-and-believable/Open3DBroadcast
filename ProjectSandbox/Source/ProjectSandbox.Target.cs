7d7e0539a62876f3f804eb1c48cfa75e8e036e81
Athomas Goldberg <athomas.goldberg@shocap.com>
2025-11-17 13:18:29 -0500

    Update to Unreal Engine 5.7 and build settings V6
    Upgraded EngineAssociation to 5.7 in the uproject file and updated DefaultBuildSettings to V6 in both Target.cs files. Also removed the PluginBuilder module from the project configuration.

--- ProjectSandbox/Source/ProjectSandbox.Target.cs
+++ ProjectSandbox/Source/ProjectSandbox.Target.cs
@@ -8,7 +8,7 @@ public class ProjectSandboxTarget : TargetRules
 	public ProjectSandboxTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
 
 		ExtraModuleNames.AddRange( new string[] { "ProjectSandbox" } );
 	}
